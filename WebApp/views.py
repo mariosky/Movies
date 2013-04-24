c1b6d40b8e2c4e19db59a2aa15bc1b5c942ed2ac
Mario Garcia Valdez <mariosky@gmail.com>
2013-04-24 13:26:32 -0700

    No Star Wars

--- WebApp/views.py
+++ WebApp/views.py
@@ -7,6 +7,6 @@ from django.http import  HttpResponse
 
 
 def movie(request, movie_id):
-    return HttpResponse("<h1>Hola Mundo, Star Wars </h1>" + movie_id  )
+    return HttpResponse("<h1>Hola Mundo, </h1>" + movie_id  )
 
 
