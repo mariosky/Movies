31b78d2456f2a0964eaca47d53cfc52d71fd1538
Mario Garcia Valdez <mariosky@gmail.com>
2013-04-24 13:27:58 -0700

    Update views.py
    Q

--- WebApp/views.py
+++ WebApp/views.py
@@ -7,6 +7,6 @@ from django.http import  HttpResponse
 
 
 def movie(request, movie_id):
-    return HttpResponse("<h1>Hola Mundo, </h1>" + movie_id  )
+    return HttpResponse("<h1>Hola Mundo, Q </h1>" + movie_id  )
 
 
